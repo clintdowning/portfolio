41b93f6478ea839a6a8bad5bc2b1bb4f296ab4ab
clintdowning <49659100+clintdowning@users.noreply.github.com>
2019-04-16 09:32:59 -0400

    Create 01_primary.php

---
+++ dropdeadmodel/content/01_primary.php
@@ -0,0 +1,49 @@
+
+				<?php require_once("includes/pic-stagger.php"); ?>
+
+                <section id="content">
+                
+                    <div class="container_12">
+
+                        <div class="row-1">
+                        
+                            <div class="wrapper">
+                            
+                                <article class="grid_4">
+                                    <h5>Out with the old</h5>
+                                    <p>There is so much talent out there in the world that is never seen, never discovered, and never realized...</p>
+                                    <p>This Modeling Agency was founded under the idea that everybody deserves an equal chance to achieve their dreams, to be seen, and to be recognized. We are a Modeling Agency that is different from the rest.</p>
+                                    <p>We aren't just another Modeling Agency where your photos are simply and carelessly tossed away into a heaping pile of other hopeful photos that never see the light of day. While you wait endlessly by the phone and waste your life away waiting for that big break.</p>
+                                    <p>Drop Dead Model is a Modeling Agency where everybody has an equal opportunity to be found, to be seen, and to reach their dreams. A Modeling Agency where you are not judged by the Agency, but rather by those who matter most, the People.</p>
+                                    <!--<p style="font-size:18px;" >- M. L. Downing (Founder)</p>
+                                    <img style="width:140px;height:auto;border-radius:25px;" src="images/mcclint_pose.jpg" alt="M. L. Downing" />-->
+                                </article>
+                                
+                                <article class="grid_4">
+                                    <h5>In with the new</h5>
+                                    <p>You don't have to pack your bags and kiss everything you know and everyone you love goodbye then head for New York, Los Angeles, Miami, or Paris anymore, in order to find out if you can get a deal and make it in modeling.</p>
+                                    <p>Drop Dead Model was founded on the principal that Everyone should have a fair chance to be seen in the world.</p>
+                                    <p>We have changed all that, leveled the playing field, and have brought to Runway to you, our Models, and All the World is your Stage...</p>
+                                    <!-- <a href="more.php" class="link-1">Read more</a> -->
+                                </article>
+                                
+                            	<article class="grid_4">
+                                    <h5>Become a model today</h5>
+                                    <p>We are looking for talent all over the world. Join us for FREE today!</p>
+									<p><a class="pro_btn pro_success pro_style_4 next_button" href="index-1-audition.php?page=_01_audition_form_input"><span></span>Make It Happen! FREE</a></p>
+                                </article>
+                            
+                            </div>
+                            
+                        </div>
+                        
+                        <div class="main">
+                            <div class="border-horiz">
+                            </div>
+                        </div>
+                        
+						<?php require_once("includes/carousel_5.php"); ?>
+                        
+                    </div>
+                    
+                </section>
