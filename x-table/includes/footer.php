76bd4ee062cf993df445179ab59de93580a85708
clintdowning <49659100+clintdowning@users.noreply.github.com>
2019-04-16 00:16:38 -0400

    Create footer.php

---
+++ x-table/includes/footer.php
@@ -0,0 +1,18 @@
+<p>
+	<a href="index.php" >Home</a> | 
+	<a href="_Multiplication_Worksheets_Interactive.php" >Interactive Mutiplication Worksheets</a> | 
+    <a href="_Multiplication_Worksheets_Printable.php" target="_blank" >Printable Mutiplication Worksheets</a>
+</p>
+<p>
+	<a href="index_04_privacy.php" >Privacy Policy</a> | 
+	<a href="index_17_contact_us.php" >Contact Us!</a>
+</p>
+<p><strong><i>Copyright <?php echo date("Y",time()); ?>  by <a href="http://<?php echo WEBSITE_NAME; ?>" ><?php echo WEBSITE_NAME; ?></a> All rights reserved.</i></strong></p>
+
+<!-- DEBUGGING -->
+
+	<?php require('includes/debug_vars.php'); ?>
+
+<!-- JS - BOTTOM -->
+
+	<? require $_SERVER['DOCUMENT_ROOT'] . '/javascript/body_end_inside.php'; ?>
